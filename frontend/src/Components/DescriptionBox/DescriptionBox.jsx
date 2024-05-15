fe9fe8014bc366ea5ab3f6465a5735752df03b39
Rajvi Pandya <rajvipandya16@gmail.com>
2024-05-15 11:56:18 +0530

    first commit

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      
+      <div className="descriptionbox-navigator">
+         
+         <div className="descriptionbox-nav-box">Description</div>
+         <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+
+         <div className="descriptionbox-description">
+            <p>
+            “Discover the epitome of style with our [Brand Name] Jacket collection. Crafted with precision and premium materials, these jackets redefine comfort and durability. Designed for versatility, they seamlessly transition from urban adventures to outdoor escapades. Featuring ample pockets and adjustable elements, our jackets offer practicality without compromising on style. Elevate your wardrobe with our signature pieces, meticulously curated for the modern explorer. Experience fashion-forward functionality with [Brand Name].”
+            </p>
+            <p>
+            “Discover the pinnacle of elegance with our [Brand Name] Jacket collection. Crafted with precision and premium materials, these jackets redefine comfort and durability. Designed for versatility, they effortlessly transition from urban adventures to outdoor escapades. Featuring numerous pockets and adjustable elements, our jackets offer practicality without compromising on style. Elevate your wardrobe with our iconic pieces, meticulously selected for the modern explorer. Experience the blend of cutting-edge functionality and fashion with [Brand Name].”
+            </p>
+         </div>
+
+    </div>
+  )
+}
+
+export default DescriptionBox
---
+++ frontend/src/Components/NewsLetter/NewsLetter.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './NewsLetter.css'
+
+
+const NewsLetter = () => {
+  return (
+    <div className='newsletter'>
+      
+          <h1>
+            Get Exclusive Offer On Your E-mail
+          </h1>
+
+          <p>
+            Subscribe to our newsletter and stay updated
+          </p>
+         <div>
+            <input type="email" placeholder='Your Email Id' />
+            <button>Subscribe</button>
+         </div>
+
+    </div>
+  )
+}
+
+export default NewsLetter
